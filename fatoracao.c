4784f81bedca30d13aff1e7fa944366ee44b9c12
Giovanni de Almeida Costa <gcosta81@hotmail.com>
2024-11-16 23:02:19 -0300

    Update fatoracao.c

--- fatoracao.c
+++ fatoracao.c
@@ -1,20 +1,12 @@
 /*
-BACHARELADO MATEMÁTICA UNIVERSIDADE POSITIVO / CRUZEIRO DO SUL
-2024-2026
-GIOVANNI DE ALMEIDA COSTA - RGM 38153386
+BACHARELADO MATEMÁTICA UNIVERSIDADE POSITIVO / CRUZEIRO DO SUL 2024-2026
+Autor: GIOVANNI DE ALMEIDA COSTA - RGM 38153386
 --- TEORIA ARITMÉTICA DOS NÚMEROS ---
 TEOREMA FUNDAMENTAL DA ARITMÉTICA - FATORAÇÃO
 */
 
 #include <stdio.h>
-
-// Estrutura para armazenar os fatores e suas quantidades
-struct Fator {
-    int valor;
-    int quantidade;
-    };
-
-void fatorar(int n, struct Fator fatores[], int *tamanho);
+#include "mathGAC.h"
 
 int main() {
     int numero;
@@ -35,23 +27,4 @@ int main() {
     printf("\n");
 
     return 0;
-}
-
-// Retorna os fatores de um inteiro qualquer
-void fatorar(int n, struct Fator fatores[], int *tamanho) {
-    *tamanho = 0; // Inicializa o tamanho do vetor de fatores
-
-    for (int i = 2; i <= n; ++i) {
-        int count = 0;
-        while (n % i == 0) {
-            n /= i;
-            ++count;
-        }
-
-        if (count > 0) {
-            fatores[*tamanho].valor = i;
-            fatores[*tamanho].quantidade = count;
-            (*tamanho)++;
-        }
-    }
 }
